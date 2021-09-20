9e68a07b7b731fb080a5f53af2b9577400649f52
Lucas Fonseca <lucasfreireri@gmail.com>
2021-09-19 22:23:17 -0300

    Created model for basic blog post

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    """A blogpost that will be displayed in the website."""
+    title = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    def __str__(self):
+        """Return a string representation of the model for the admin"""
+        return self.title
