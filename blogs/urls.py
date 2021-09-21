11e0e7497fe5ccb408bd344eabb262d76364418d
Lucas Fonseca <lucasfreireri@gmail.com>
2021-09-21 08:48:48 -0300

    Created page to edit existing posts

--- blogs/urls.py
+++ blogs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page for adding a new Blog Post
     path('new_post/', views.new_post, name='new_post'),
+    # Page for editing a Blog Post
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -24,3 +24,21 @@ def new_post(request):
     # Display a blank or invalid form
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
+
+def edit_post(request, post_id):
+    """Edit an existing post"""
+    post = BlogPost.objects.get(id=post_id)
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current post.
+        form = BlogPostForm(instance=post)
+    else:
+        # POST data submitted; process data.
+        form = BlogPostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:index')
+    
+    context = {'post': post, 'form': form}
+    return render(request, 'blogs/edit_post.html', context)
+
